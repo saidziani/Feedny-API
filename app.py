371b75f461f4abfaa1a72425eed984110b7a2a35
Said <saidziani.pro@gmail.com>
2018-04-13 19:41:57 +0100

    Add: method to create new raw.

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ def getPeople():
         abort(404)
     return jsonify({'people': people})
 
-
+	
 # Get one person by id
 @app.route('/api/people/id=<int:person_id>', methods=['GET'])
 # curl -i http://localhost:5000/api/people/id=2
@@ -36,3 +36,21 @@ def getPerson(person_id):
     if len(person) == 0:
         notFound(abort(404))
     return jsonify({'person': person})
+
+
+# Create new person <name> required
+@app.route('/api/people/', methods=['POST'])
+# curl -i -H "Content-Type: application/json" -X POST -d '{"name":"Poo"}' http://localhost:5000/api/people/
+def createPerson():
+    if not request.json or not 'name' in request.json:
+        badRequest(abort(400))
+    person = {
+        'id': people[-1]['id'] + 1,
+        'name': request.json['name'],
+        'age': request.json.get('age', 0),
+        'sexe': request.json.get('sexe', ''),
+        'student': False
+    }
+    people.append(person)
+    return jsonify({'person': person}), 201
+
