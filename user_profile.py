ca1ab0d9ef6e8480713ae7031ec7d5769702f502
Said <saidziani.pro@gmail.com>
2018-05-17 10:59:01 +0100

    Add password.

--- user_profile.py
+++ user_profile.py
@@ -2,17 +2,17 @@
 
 
 class UserProfile():
-    def __init__(self, id=None, username=None, categories=None, sources=None):
-        self.id = id 
+    def __init__(self, username=None, password=None, categories=None, sources=None):
         self.username = username
+        self.password = password
         self.categories = categories 
         self.sources = sources
 
 
-    def getNewsArticle(self):
+    def getUserProfile(self):
         return {
-            'id' : self.id, 
             'username' : self.username,
+            'password' : self.password,
             'categories' : self.categories, 
             'sources' : self.sources
         }
